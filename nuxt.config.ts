a3d8844d224069537bbbd7b1b7ff8b6434998fc5
RobArkesteijn <rarkesteijn@outlook.com>
2023-08-29 22:00:48 +0200

    further setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  srcDir: 'src/',
+  app: {
+    head: {
+      title: 'Rob Arkesteijn',
+      link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }],
+      viewport: 'width=device-width, initial-scale=1',
+    },
+  },
+  css: ['@/assets/scss/main.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/scss/_variables.scss" as *;',
+        },
+      },
+    },
+  },
+});
---
+++ src/components/AnimatedStripes.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="animated-stripes">
+    <div
+      v-for="index in 4"
+      :key="`stripe-${index}`"
+      class="animated-stripes__line"
+    ></div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.animated-stripes {
+  position: absolute;
+  top: 0;
+  display: flex;
+  height: 100vh;
+
+  &__line {
+    width: 80vw;
+    height: 5px;
+    background-color: $powder-blue;
+  }
+}
+</style>
