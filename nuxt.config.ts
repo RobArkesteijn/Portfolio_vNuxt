b0bd38bc6cb5eeecc73a9604f9d4a1e6b27b80a5
RobArkesteijn <rarkesteijn@outlook.com>
2024-07-27 19:10:38 +0200

    Upgrade to latest major package version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,96 +1,75 @@
-import svgLoader from 'vite-svg-loader';
+import svgLoader from 'vite-svg-loader'
+
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  srcDir: 'src/',
-  css: ['@/assets/scss/main.scss'],
-  vite: {
-    plugins: [svgLoader()],
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: '@use "@/assets/scss/_variables.scss" as *;',
-        },
+  $production: {
+    pwa: {
+      manifest: {
+        name: 'R.A. | Portfolio',
+        short_name: 'Portfolio',
+        description:
+          'Rob Arkesteijn\'s personal website. Where he shares his passion for software development. A showcase of his projects, skills, and experience.',
+        icons: [
+          {
+            src: '/pwa/icon-64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa/icon-144.png',
+            sizes: '144x144',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        display: 'fullscreen',
+        theme_color: '#1b3f4d',
+        background_color: '#232929',
+        start_url: '../index.html',
+      },
+      workbox: {
+        navigateFallback: '/',
+      },
+      devOptions: {
+        enabled: true,
+        type: 'module',
       },
     },
   },
+
+  compatibilityDate: '2024-07-27',
+
   components: [
     {
-      path: '~/components',
+      path: '@/components',
       pathPrefix: false,
     },
   ],
-  modules: [
-    '@nuxtjs/i18n',
-    'nuxt-icon',
-    'vue3-carousel-nuxt',
-    '@nuxt/image',
-    '@hypernym/nuxt-gsap',
-    'nuxt-og-image',
-    '@nuxtjs/robots',
-    '@vite-pwa/nuxt',
-    '@nuxtjs/sitemap',
-  ],
-  image: {
-    dir: 'assets/images',
-    quality: 80,
-  },
-  pwa: {
-    manifest: {
-      name: 'R.A. | Portfolio',
-      short_name: 'Portfolio',
-      description:
-        "Rob Arkesteijn's personal website. Where he shares his passion for software development. A showcase of his projects, skills, and experience.",
-      icons: [
-        {
-          src: '/pwa/icon-64.png',
-          sizes: '64x64',
-          type: 'image/png',
-        },
-        {
-          src: '/pwa/icon-144.png',
-          sizes: '144x144',
-          type: 'image/png',
-        },
-        {
-          src: '/pwa/icon-192.png',
-          sizes: '192x192',
-          type: 'image/png',
-        },
-        {
-          src: '/pwa/icon-512.png',
-          sizes: '512x512',
-          type: 'image/png',
-        },
-      ],
-      display: 'fullscreen',
-      theme_color: '#1b3f4d',
-      background_color: '#232929',
-      start_url: '../index.html',
-    },
-    workbox: {
-      navigateFallback: '/',
-    },
-    devOptions: {
-      enabled: true,
-      type: 'module',
+
+  css: ['@/assets/scss/main.scss'],
+
+  devtools: { enabled: true },
+
+  eslint: {
+    config: {
+      stylistic: true,
     },
   },
-  robots: {
-    UserAgent: '*',
-    Disallow: '',
+
+  future: {
+    compatibilityVersion: 4,
   },
-  site: {
-    url: process.env.NUXT_SITE_URL,
-  },
-  ogImage: {
-    fonts: ['Spectral:200'],
-  },
-  gsap: {
-    composables: true,
-  },
+
   i18n: {
-    baseUrl: 'https://www.robarkes.nl',
-    vueI18n: './i18n.config.ts',
+    baseUrl: import.meta.env.NUXT_PUBLIC_BASE_URL,
     defaultLocale: 'nl',
     locales: [
       {
@@ -125,6 +104,31 @@ export default defineNuxtConfig({
       useCookie: false,
     },
   },
+
+  image: {
+    dir: 'assets/images',
+    quality: 80,
+  },
+
+  modules: [
+    '@nuxtjs/i18n',
+    'vue3-carousel-nuxt',
+    '@nuxt/image',
+    '@vite-pwa/nuxt',
+    '@nuxtjs/seo',
+    '@nuxt/eslint',
+    '@nuxt/icon',
+  ],
+
+  ogImage: {
+    fonts: ['Spectral:200'],
+  },
+
+  robots: {
+    UserAgent: '*',
+    Disallow: '',
+  },
+
   sitemap: {
     xslColumns: [
       { label: 'URL', width: '50%' },
@@ -132,4 +136,15 @@ export default defineNuxtConfig({
       { label: 'Hreflangs', select: 'count(xhtml)', width: '25%' },
     ],
   },
-});
+
+  vite: {
+    plugins: [svgLoader()],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/scss/_variables.scss" as *;',
+        },
+      },
+    },
+  },
+})
